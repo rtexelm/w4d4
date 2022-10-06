0bb22d369654f9db6e50693005f2c31de5b30c6f
rtexelm <texelist@gmail.com>
2022-10-06 15:28:29 -0400

    Added my_transpose method tests

--- test_driven_development/lib/ttd_1.rb
+++ test_driven_development/lib/ttd_1.rb
@@ -6,4 +6,29 @@ class Array
         end
         hash.keys
     end
+
+    def two_sum
+        result = []
+        (0...self.length-1).each do |i|
+            (i+1...self.length).each do |j|
+                if self[i] + self[j] == 0
+                    result << [i, j]
+                end
+            end
+        end
+        result
+    end
+
+    def my_transpose
+        # return self if self.empty?
+        raise "2D array must be square" if !self.all?{|sub| sub.length == self.length}
+        result = Array.new(self.length) {Array.new(self.length)}
+        (0...self.length).each do |i|
+            (0...self.length).each do |j|
+                result[i][j] = self[j][i]
+            end
+        end
+        result
+    end
 end
+
--- test_driven_development/spec/ttd_1_spec.rb
+++ test_driven_development/spec/ttd_1_spec.rb
@@ -18,11 +18,46 @@ describe Array do
     describe "#two_sum" do
 
         it "should find all pairs of pos where the elements at pos sum to 0" do
-            expect(array.two_sum).to eq([[0, 4], [2, 3]])
+            expect(array.two_sum).to match_array([[0, 4], [2, 3]]).or match_array([[4, 0], [3, 2]]).or match_array([[0, 4], [3, 2]]).or match_array([[4, 0], [2, 3]])
         end
 
         it "should return pairs in a sorted order where smaller index is before bigger index" do
             expect(array.two_sum).to_not eq([[2, 3], [0, 4]])
+            expect(array.two_sum).to eq([[0, 4], [2, 3]])
+        end
+    end
+end
+
+
+describe Array do
+    subject(:array) {[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+        ]}
+    subject(:array2) {[
+        [0, 1, 2],
+        [3, 4, 5]
+        ]}
+    subject(:array3) {[]}
+
+
+    describe "#my_transpose" do
+
+        it "should return itself when it is an empty array" do
+            expect(array3.my_transpose).to eq([])
+        end
+
+        it "should accept a square 2d array" do
+            expect{array2.my_transpose}.to raise_error("2D array must be square")
+        end
+
+        it "should return a new 2d array with the rows and cols switched" do
+            expect(array.my_transpose).to eq([
+                                                [0, 3, 6],
+                                                [1, 4, 7],
+                                                [2, 5, 8]
+                                            ])
         end
     end
 end
